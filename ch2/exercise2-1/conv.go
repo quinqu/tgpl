1140ca864da22dab7f971db2b5b589bbc268c6e2
Quin Quintero <quinq@gravitational.com>
2020-08-11 16:47:52 -0700

    added K to C and K to F methods

--- ch2/exercise2-1/conv.go
+++ ch2/exercise2-1/conv.go
@@ -6,9 +6,16 @@ func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 // FToC converts a Fahrenheit temperature to Celsius.
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
-func CToK(c Celsius) Kelvin { return Kelvin(c + AbsoluteZeroC) }
+func CToK(c Celsius) Kelvin { return Kelvin(c - AbsoluteZeroC) }
 
 func FToK(f Fahrenheit) Kelvin {
 	c := (FToC(f))
 	return Kelvin(c - AbsoluteZeroC)
 }
+
+func KToF(k Kelvin) Fahrenheit {
+	c := KToC(k)
+	return Fahrenheit(CToF(c))
+}
+
+func KToC(k Kelvin) Celsius { return Celsius(k - 273.15) }
