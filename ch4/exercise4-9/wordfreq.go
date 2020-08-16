733f902bd39a4449381f6ccfc64359ad26d09677
Quin Quintero <quinq@gravitational.com>
2020-08-16 11:36:45 -0700

    word count with map

---
+++ ch4/exercise4-9/wordfreq.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	
+)
+
+func main() {
+	wordfreq("wordfreq.txt")
+}
+
+func wordfreq(file string) {
+	wordCounts := make(map[string]int)
+    f, _ := os.Open(file)
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		currentWord := scanner.Text()
+		_, found := wordCounts[currentWord]
+
+		if found {
+			wordCounts[currentWord]++
+		} else {
+			wordCounts[currentWord] = 1 
+		}
+	}
+
+	for word, count := range wordCounts {
+		fmt.Println(word, ": ", count)
+	}
+}
