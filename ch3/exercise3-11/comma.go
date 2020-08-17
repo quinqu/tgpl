4ef0ceb2ab5e65a116260b66b4fe72f5903444de
Quin Quintero <quinq@gravitational.com>
2020-08-17 15:58:21 -0700

    can handle floats

--- ch3/exercise3-11/comma.go
+++ ch3/exercise3-11/comma.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"regexp"
+	"strings"
 )
 
 func main() {
@@ -14,9 +17,44 @@ func main() {
 //!+
 // comma inserts commas in a non-negative decimal integer string.
 func comma(s string) string {
-	n := len(s)
-	if n <= 3 {
+
+	match, _ := regexp.MatchString("\\.", s) //true or false
+
+	var splitString []string
+	splitString = strings.Split(s, ".")
+
+	if len(splitString[0]) <= 3 {
 		return s
 	}
-	return comma(s[:n-3]) + "," + s[n-3:]
+
+	if match {
+		buf := insertCommas(splitString[0])
+		buf.WriteString("." + splitString[1])
+		return buf.String()
+	}
+
+	buf := insertCommas(s)
+	return buf.String()
+}
+
+func insertCommas(s string) bytes.Buffer {
+
+	var buf bytes.Buffer
+	n := len(s)
+
+	offset := n % 3
+	if offset > 0 {
+		buf.WriteString(s[0:offset])
+		buf.WriteByte(',')
+	}
+	s = s[offset:]
+
+	for i := 0; i < len(s); i += 3 {
+		buf.WriteString(s[i : i+3])
+		if i < len(s)-3 {
+			buf.WriteByte(',')
+		}
+
+	}
+	return buf
 }
