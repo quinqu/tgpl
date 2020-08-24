b960f2165a8f2bec555a2db9caaae6c0a24816f8
Quin Quintero <quinq@gravitational.com>
2020-08-24 10:03:11 -0700

    main

--- ch5/exercise5-9/expand.go
+++ ch5/exercise5-9/expand.go
@@ -10,10 +10,25 @@ const (
 )
 
 func main() {
-	fmt.Println(expand("$foo $foo $fo", f))
+	fmt.Println(expand("$foo $foo $fo", func(x string)string {
+		return "random"
+	})) 
+
+	fmt.Println(expand("", func(x string)string {
+		return "random"
+	}))
+	
+	fmt.Println(expand("$hello, $foo", func(x string)string {
+		return "hello"
+	}))
+
 }
 
 func expand(s string, f func(string) string) string {
+	if len(s) < 1 {
+		return s 
+	}
+
 	elements := strings.Split(s, " ")
 
 	for i := range elements {
@@ -25,6 +40,3 @@ func expand(s string, f func(string) string) string {
 	return strings.Join(elements, " ")
 }
 
-func f (s string) string {
-	return s[0:1]
-}
