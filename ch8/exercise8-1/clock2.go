4285d1a097688be0a398e42287184d96163f6e79
Quin Quintero <quinq@gravitational.com>
2020-09-14 15:39:12 -0700

    remove timezone flag

--- ch8/exercise8-1/clock2.go
+++ ch8/exercise8-1/clock2.go
@@ -1,3 +1,4 @@
+ 
 package main
 
 import (
@@ -6,49 +7,35 @@ import (
 	"net"
 	"time"
 	"flag"
-	
 )
 //creating servers 
 
-func handleConn(c net.Conn, timezone string) {
+func handleConn(c net.Conn) {
 	defer c.Close()
-	loc, err := time.LoadLocation(timezone)
-	if err != nil {
-		return 
-	}
-	
 	for {
-		_, err := io.WriteString(c, time.Now().In(loc).Format("15:04:05\n"))
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
 		if err != nil {
 			return // e.g., client disconnected
 		}
 		time.Sleep(1 * time.Second)
 	}
 }
-
 var p = flag.String("port", "8000", "desired port number")
-var TZ = flag.String("timezone", "US/Eastern", "examples: US/Eastern, Asia/Tokyo, Europe/London")
+
 func main() {
 	
 	flag.Parse()
 	host := "localhost:" + *p
-	timezone := *TZ
 	listener, err := net.Listen("tcp", host)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	
-	//!+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Print(err) // e.g., connection aborted
 			continue
 		}
-		go handleConn(conn, timezone) // handle connections concurrently
+		go handleConn(conn) // handle connections concurrently
 	}
-	//!-
 }
-
-
