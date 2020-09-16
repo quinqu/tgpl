52560c77956bbe454d08447fd9437db0b209443e
Quin Quintero <quinq@gravitational.com>
2020-09-15 18:29:41 -0700

    depth flag

--- ch8/exercise8-6/crawl3.go
+++ ch8/exercise8-6/crawl3.go
@@ -2,34 +2,46 @@ package main
 
 import (
 	"./links"
+	"flag"
 	"fmt"
 	"log"
-	"os"
 )
 
-func crawl(url string) []string {
+var depth = flag.Int("depth", 1, "Only URLs reachable by at most `depth` links will be fetched")
+
+func crawl(url string, n int) ([]string, int) {
 	fmt.Println(url)
 	list, err := links.Extract(url)
 	if err != nil {
 		log.Print(err)
 	}
-	return list
+	n++
+	return list, n
 }
 
 //!+
 func main() {
+	flag.Parse()
 	worklist := make(chan []string)  // lists of URLs, may have duplicates
 	unseenLinks := make(chan string) // de-duplicated URLs
-
+	var n int
 	// Add command-line arguments to worklist.
-	go func() { worklist <- os.Args[1:] }()
-
+	go func() {
+		worklist <- flag.Args()
+	}()
+	
 	// Create 20 crawler goroutines to fetch each unseen link.
 	for i := 0; i < 20; i++ {
 		go func() {
+			
 			for link := range unseenLinks {
-				foundLinks := crawl(link)
-				go func() { worklist <- foundLinks }()
+				
+				foundLinks, newN := crawl(link, n)
+				n = newN
+				go func() {
+					
+					worklist <- foundLinks
+				}()
 			}
 		}()
 	}
@@ -37,12 +49,21 @@ func main() {
 	// The main goroutine de-duplicates worklist items
 	// and sends the unseen ones to the crawlers.
 	seen := make(map[string]bool)
-	for list := range worklist {
-		for _, link := range list {
+	
+	for n < *depth {
+		list := <-worklist
+		for _, link := range list { //here worklist is a channel
 			if !seen[link] {
 				seen[link] = true
 				unseenLinks <- link
 			}
 		}
+		
 	}
+
+	if n >= *depth {
+		close(unseenLinks)
+	}
+	//}
+
 }
